b2b58c1aba9a411413877bc9874c0cb120a3510a
JinalBhanushali0628 <jinalbhanushali0628@gmail.com>
2021-02-28 15:25:03 -0500

    first commit

--- backend/controller/UserAuthController.js
+++ backend/controller/UserAuthController.js
@@ -6,7 +6,6 @@ const catchAsyncError = require("../middleware/catchAsyncError")
 exports.UserRegister = catchAsyncError ( async (req, res, next)=>{
 
     const { name, email, password} = req.body
-
     const user= await User.create({
         name,
         email,
